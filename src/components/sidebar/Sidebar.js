8a2daff17a9aad44f9cbaede28b61197153c20e2
nstone213 <148160623+nstone213@users.noreply.github.com>
2025-01-19 13:23:25 -0500

    Moved buttons to sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,10 +2,17 @@ import React from "react";
 import "./Sidebar.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const Sidebar = () => {
+const Sidebar = ({ handleSignOut, setShowResults }) => {
+
   return (
     <div className="sidebar">
-      <p>HELLOOOOO</p>
+        <p>HELLOOOOO</p>
+        <button onClick={() => setShowResults(true)} className="results-button">
+            Results <i className="fas fa-check-to-slot"></i>
+        </button>
+        <button onClick={handleSignOut} className="sign-out-button">
+            Sign Out <i className="fas fa-right-from-bracket"></i>
+        </button>
     </div>
   );
 };
--- src/components/userinfo/UserInfo.js
+++ src/components/userinfo/UserInfo.js
@@ -3,7 +3,7 @@ import "./UserInfo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Sidebar from '../sidebar/Sidebar';
 
-const UserInfo = ({ name, user, handleSignOut, setShowResults }) => {
+const UserInfo = ({ name }) => {
   const [isSidebarVisible, setIsSidebarVisible] = useState(false);
 
   return (
@@ -12,12 +12,6 @@ const UserInfo = ({ name, user, handleSignOut, setShowResults }) => {
         <div className="user-details">
           {name && <div className="user-name">{name}</div>}
         </div>
-        <button onClick={() => setShowResults(true)} className="results-button">
-          Results <i className="fas fa-check-to-slot"></i>
-        </button>
-        <button onClick={handleSignOut} className="sign-out-button">
-          Sign Out <i className="fas fa-right-from-bracket"></i>
-        </button>
         <button
           className="menu-button"
           onClick={() => setIsSidebarVisible(!isSidebarVisible)}
