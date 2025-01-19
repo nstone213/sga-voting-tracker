bd3807ae3a95a9e5406cf6795c312f58d92c9ff1
nstone213 <148160623+nstone213@users.noreply.github.com>
2025-01-18 20:17:26 -0500

    Grayed out and made objects unusuable behind results popup when it is in use

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -3,17 +3,20 @@ import "./Results.css";
 
 const Results = ({ votes, setShowResults }) => {
   return (
-    <div className="results-modal">
-      <button onClick={() => setShowResults(false)} className="close-button">
-        ✖
-      </button>
-      <div className="votes-container">
-        {Object.entries(votes).map(([uid, data]) => (
-          <div key={uid} className={`vote-box ${data.vote}`} title={data.name}></div>
-        ))}
+    <>
+      <div className="modal-overlay" onClick={() => setShowResults(false)}></div>
+      <div className="results-modal">
+        <button onClick={() => setShowResults(false)} className="close-button">
+          ✖
+        </button>
+        <div className="votes-container">
+          {Object.entries(votes).map(([uid, data]) => (
+            <div key={uid} className={`vote-box ${data.vote}`} title={data.name}></div>
+          ))}
+        </div>
+        <button className="settings-button">Settings</button>
       </div>
-      <button className="settings-button">Settings</button>
-    </div>
+    </>
   );
 };
 
