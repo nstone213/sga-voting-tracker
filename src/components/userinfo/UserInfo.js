0df7e2d742f143a2436358bd7fb7898d2524e541
nstone213 <148160623+nstone213@users.noreply.github.com>
2025-01-18 21:02:46 -0500

    Added icons to UserInfo buttons

--- src/components/userinfo/UserInfo.js
+++ src/components/userinfo/UserInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./UserInfo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const UserInfo = ({ name, user, handleSignOut, setShowResults }) => {
   return (
@@ -7,10 +8,10 @@ const UserInfo = ({ name, user, handleSignOut, setShowResults }) => {
       {name && <div className="user-name">{name}</div>}
       <p className="user-id">User ID: {user?.uid}</p>
       <button onClick={() => setShowResults(true)} className="results-button">
-        Results
+        <i className="fas fa-check-to-slot"></i>
       </button>
       <button onClick={handleSignOut} className="sign-out-button">
-        Sign Out
+        <i className="fas fa-right-from-bracket"></i>
       </button>
     </div>
   );
