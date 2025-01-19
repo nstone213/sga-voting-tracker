e7990a3f7ddc395c0a70701a60458d964f6badc2
nstone213 <148160623+nstone213@users.noreply.github.com>
2025-01-19 15:41:44 -0500

    UI Change

--- src/components/userinfo/UserInfo.js
+++ src/components/userinfo/UserInfo.js
@@ -12,21 +12,29 @@ const UserInfo = ({ name, user, handleSignOut, setShowResults }) => {
         <div className="user-details">
           {name && <div className="user-name">{name}</div>}
         </div>
-        <button onClick={() => setShowResults(true)} className="results-button">
-          Results <i className="fas fa-check-to-slot"></i>
-        </button>
-        <button onClick={handleSignOut} className="sign-out-button">
-          Sign Out <i className="fas fa-right-from-bracket"></i>
-        </button>
-        <button
-          className="menu-button"
-          onClick={() => setIsSidebarVisible(!isSidebarVisible)}
-        >
-          <i className="fas fa-bars"></i>
-        </button>
+        
+        <div className="title">
+          <h2>
+            UHR Voting Dashboard
+          </h2>
+        </div>
+        {/* Buttons grouped together on the right */}
+        <div className="button-group">
+          <button onClick={() => setShowResults(true)} className="results-button">
+            Results <i className="fas fa-check-to-slot"></i>
+          </button>
+          <button onClick={handleSignOut} className="sign-out-button">
+            Sign Out <i className="fas fa-right-from-bracket"></i>
+          </button>
+          <button
+            className="menu-button"
+            onClick={() => setIsSidebarVisible(!isSidebarVisible)}
+          >
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
       </div>
 
-      {/* Sidebar appears as an overlay */}
       {isSidebarVisible && <Sidebar />}
     </>
   );
