09bb6b60148276d1cd83f996800ed585212b0188
nstone213 <148160623+nstone213@users.noreply.github.com>
2025-01-19 13:17:55 -0500

    Sidebar styling

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Sidebar.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <p>HELLOOOOO</p>
+    </div>
+  );
+};
+
+export default Sidebar;
