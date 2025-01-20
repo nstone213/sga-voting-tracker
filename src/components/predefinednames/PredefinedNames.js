1f053c397a911a3750357662ba996644ea89ad86
nstone213 <148160623+nstone213@users.noreply.github.com>
2025-01-20 01:56:43 -0500

    Offloaded predefined names

---
+++ src/components/predefinednames/PredefinedNames.js
@@ -0,0 +1,22 @@
+const predefinedNames = [
+    "as, aS", 
+    "DeBord, Wyatt", 
+    "Charlie Lee", 
+    "Dana White", 
+    "Pastula, Noah", 
+    "R., Dorian", 
+    "asdf", 
+    "SSSS", 
+    "JKLJ", 
+    "New Nae 1", 
+    "New Name 2", 
+    "New Name 3", 
+    "New Name 4", 
+    "New Name 5", 
+    "New Name 6", 
+    "New Name 7", 
+    "New Name 8", 
+    "New Name 9"
+];
+
+export default predefinedNames;
--- src/components/rollcall/RollCall.js
+++ src/components/rollcall/RollCall.js
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "../firebaseconfig/firebaseConfig";
+import predefinedNames from "../predefinednames/PredefinedNames"; // Import the predefined names
 import "./RollCall.css"; 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const RollCall = () => {
   const [loggedInUsers, setLoggedInUsers] = useState([]);
-
-  // Predefined list of names to check against
-  const predefinedNames = ["as, aS", "DeBord, Wyatt", "Charlie Lee", "Dana White", "Pastula, Noah", "R., Dorian", "asdf", "SSSS", "JKLJ", 
-    "New Name 1", "New Name 2", "New Name 3", "New Name 4", "New Name 5", "New Name 6", "New Name 7", "New Name 8", "New Name 9"]; // Added extra names for testing
 
   useEffect(() => {
     const unsubscribe = onSnapshot(collection(db, "users"), (snapshot) => {
