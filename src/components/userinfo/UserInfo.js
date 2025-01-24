acbb6d09319db6c2de3a6c75662ec7b9261d424c
nstone213 <148160623+nstone213@users.noreply.github.com>
2025-01-24 14:21:44 -0500

    Sign out button moved into sidebar

--- src/components/userinfo/UserInfo.js
+++ src/components/userinfo/UserInfo.js
@@ -33,9 +33,6 @@ const UserInfo = ({ name, handleSignOut, isDarkMode }) => {
         </div>
 
         <div className="button-group">
-          <button onClick={handleSignOut} className="sign-out-button">
-            Sign Out <i className="fas fa-right-from-bracket"></i>
-          </button>
           <button
             className="menu-button"
             onClick={() => setIsSidebarVisible(!isSidebarVisible)}
@@ -46,7 +43,7 @@ const UserInfo = ({ name, handleSignOut, isDarkMode }) => {
       </div>
 
       {/* Sidebar */}
-      <Sidebar isSidebarVisible={isSidebarVisible} closeSidebar={closeSidebar} openPopup={openPopup} />
+      <Sidebar isSidebarVisible={isSidebarVisible} closeSidebar={closeSidebar} openPopup={openPopup} handleSignOut={handleSignOut} />
 
       {/* Password Popup */}
       {isPopupVisible && <PasswordPopup closePopup={closePopup} />}
