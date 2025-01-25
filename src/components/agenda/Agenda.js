7c14f23fa3d31679bd865ca763a1887f66b1b9d3
nstone213 <148160623+nstone213@users.noreply.github.com>
2025-01-24 23:54:59 -0500

    Agenda UI

--- src/components/agenda/Agenda.js
+++ src/components/agenda/Agenda.js
@@ -6,6 +6,47 @@ const Agenda = () => {
   return (
     <div className="agenda-container">
         <p className="agenda-title">Undergraduate House of Representatives Agenda</p>
+        <p>
+          DATE
+        </p>
+        <div className="agenda-items">
+          <p>
+            1. Call to Order
+          </p>
+          <p>
+            2. SGA Mission Statement
+          </p>
+          <p>
+            3. Buzz of the Week!
+          </p>
+          <p>
+            4. Cabinet Updates
+          </p>
+          <p>
+            5. Presentations
+          </p>
+          <p>
+            6. Open Forum
+          </p>
+          <p>
+            7. Old Business
+          </p>
+          <p>
+            8. New Business
+          </p>
+          <p>
+            9. Special Topics
+          </p>
+          <p>
+            10. Announcements
+          </p>
+          <p>
+            11. Presidential Review
+          </p>
+          <p>
+            12. Adjournment
+          </p>
+        </div>
     </div>
   );  
 };
