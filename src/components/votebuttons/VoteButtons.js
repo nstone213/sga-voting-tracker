69d775c2b3d307e02049de221757d4b1542a0dfc
nstone213 <148160623+nstone213@users.noreply.github.com>
2025-01-24 20:39:30 -0500

    UI Updates

--- src/components/votebuttons/VoteButtons.js
+++ src/components/votebuttons/VoteButtons.js
@@ -28,7 +28,7 @@ const VoteButtons = ({ user, handleVote }) => {
   return (
     <div className="voting-container">
         <div className="voting-details">
-            <p>Voting in progress...</p>
+            <p>asfjfjfffjff in progress...</p>
             <p>xx:xx:xx</p>
         </div>
         <h2 className="bill-title">BILL TBD</h2>
