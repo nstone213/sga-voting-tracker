8fedcef4c9555347077314a60b77226947ab4c6a
nstone213 <148160623+nstone213@users.noreply.github.com>
2025-01-24 20:27:52 -0500

    Live Updates initialized

---
+++ src/components/liveupdates/LiveUpdates.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./LiveUpdates.css";
+
+
+const LiveUpdates = () => {
+  
+  return (
+    <div className='liveupdates-container'>
+      <p>Live Updates</p>
+    </div>
+  );
+};
+
+export default LiveUpdates;
